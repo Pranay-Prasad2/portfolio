8fc08601aeed0cef0e624b8ca0a37d23c22e6b1c
Pranay Prasad <pranayprasad42+work@gmail.com>
2025-03-02 01:34:49 +0000

    final frontenddesgin

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,10 @@
+import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
+import RecentProject from "@/components/RecentProject";
 import { FloatingNav } from "@/components/UI/FloatingNav";
+import WorkExp from "@/components/WorkExp";
+import { navItems } from "@/Data";
 import Image from "next/image";
 import { FaHome } from "react-icons/fa";
 
@@ -8,11 +12,12 @@ export default function Home() {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        {/* <FloatingNav navItems={[
-          {name: "Home", link: "/", icon: <FaHome/>}
-        ]}/> */}
+        <FloatingNav navItems={navItems}/>
         <Hero/>
         <Grid/>
+        <RecentProject/>
+        <WorkExp/>
+        <Footer/>
       </div>
     </main>
   );
