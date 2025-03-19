f80a710de2ea5d208f00f45e905ef5d982a154f3
Pranay Prasad <pranayprasad42+work@gmail.com>
2025-03-19 11:17:44 +0000

    data fix

--- components/UI/Globe.tsx
+++ components/UI/Globe.tsx
@@ -5,7 +5,7 @@ import ThreeGlobe from "three-globe";
 import { useThree, Canvas, extend } from "@react-three/fiber";
 
 import { OrbitControls } from "@react-three/drei";
-import countries from "@/data/globe.json";
+import countries from '../../Data/globe.json'
 
 declare module "@react-three/fiber" {
   interface ThreeElements {
