1e703d0dc50a521ee57c6a90a9c2ba12aab97da0
Pranay Prasad <pranayprasad42+work@gmail.com>
2025-03-20 20:55:05 +0000

    fixes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -16,6 +16,17 @@ const config = {
     "./src/**/*.{ts,tsx}",
     "./data/**/*.{ts,tsx}",
   ],
+  safelist: [
+    "grid-cols-1",
+    "grid-cols-2",
+    "grid-cols-3",
+    "grid-cols-4",
+    "grid-cols-5",
+    "grid-cols-6",
+    "lg:grid-cols-3",
+    "md:grid-cols-2",
+    "sm:grid-cols-1",
+  ],
   prefix: "",
   theme: {
     container: {
