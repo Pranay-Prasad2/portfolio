f6fbdefc254635f6296509932cb3d71fe5a6d79e
Pranay Prasad <pranayprasad42+work@gmail.com>
2025-03-20 21:03:57 +0000

    next-config

--- next.config.ts
+++ next.config.ts
@@ -1,47 +1,80 @@
-import {withSentryConfig} from "@sentry/nextjs";
+// import {withSentryConfig} from "@sentry/nextjs";
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+//   output: 'export',
+//   typescript:{
+//     ignoreBuildErrors: true,
+//   }
+//   sentry: process.env.NEXT_PUBLIC_DISABLE_SENTRY === "true" ? undefined : {},
+// };
+
+// export default withSentryConfig(nextConfig, {
+// // For all available options, see:
+// // https://www.npmjs.com/package/@sentry/webpack-plugin#options
+
+// org: "prasads-xt",
+// project: "javascript-nextjs",
+
+// // Only print logs for uploading source maps in CI
+// silent: !process.env.CI,
+
+// // For all available options, see:
+// // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+// // Upload a larger set of source maps for prettier stack traces (increases build time)
+// widenClientFileUpload: true,
+
+// // Automatically annotate React components to show their full name in breadcrumbs and session replay
+// reactComponentAnnotation: {
+// enabled: true,
+// },
+
+// // Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
+// // This can increase your server load as well as your hosting bill.
+// // Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
+// // side errors will fail.
+// // tunnelRoute: "/monitoring",
+
+// // Automatically tree-shake Sentry logger statements to reduce bundle size
+// disableLogger: true,
+
+// // Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
+// // See the following for more information:
+// // https://docs.sentry.io/product/crons/
+// // https://vercel.com/docs/cron-jobs
+// automaticVercelMonitors: true,
+// });
+
+
+
+import { withSentryConfig } from "@sentry/nextjs";
 import type { NextConfig } from "next";
 
+const isNetlify = process.env.NETLIFY === "true"; // Detect Netlify
+
 const nextConfig: NextConfig = {
-  /* config options here */
-  output: 'export',
-  typescript:{
+  output: "export", // Static export (breaks server features like API routes)
+  typescript: {
     ignoreBuildErrors: true,
-  }
+  },
+  images: {
+    unoptimized: true, // Required for `next export`
+  },
+  trailingSlash: true, // Fixes Netlify's issue with missing `/index.html`
+  sentry: isNetlify ? undefined : {}, // Disable Sentry on Netlify
 };
 
-export default withSentryConfig(nextConfig, {
-// For all available options, see:
-// https://www.npmjs.com/package/@sentry/webpack-plugin#options
-
-org: "prasads-xt",
-project: "javascript-nextjs",
-
-// Only print logs for uploading source maps in CI
-silent: !process.env.CI,
-
-// For all available options, see:
-// https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-// Upload a larger set of source maps for prettier stack traces (increases build time)
-widenClientFileUpload: true,
-
-// Automatically annotate React components to show their full name in breadcrumbs and session replay
-reactComponentAnnotation: {
-enabled: true,
-},
-
-// Uncomment to route browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers.
-// This can increase your server load as well as your hosting bill.
-// Note: Check that the configured route will not match with your Next.js middleware, otherwise reporting of client-
-// side errors will fail.
-// tunnelRoute: "/monitoring",
-
-// Automatically tree-shake Sentry logger statements to reduce bundle size
-disableLogger: true,
-
-// Enables automatic instrumentation of Vercel Cron Monitors. (Does not yet work with App Router route handlers.)
-// See the following for more information:
-// https://docs.sentry.io/product/crons/
-// https://vercel.com/docs/cron-jobs
-automaticVercelMonitors: true,
-});
+// Apply Sentry only when NOT exporting statically
+export default isNetlify
+  ? nextConfig
+  : withSentryConfig(nextConfig, {
+      org: "prasads-xt",
+      project: "javascript-nextjs",
+      silent: !process.env.CI,
+      widenClientFileUpload: true,
+      reactComponentAnnotation: { enabled: true },
+      disableLogger: true,
+      automaticVercelMonitors: true,
+    });
